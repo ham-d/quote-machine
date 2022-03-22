0f6ae614e798fda80660b92820ee7c95ca2b3882
Daniel Ham <danieljham@gmail.com>
2022-03-21 21:18:45 -0700

    initial commit

---
+++ src/components/quotes.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const Quotes = (): JSX.Element => {
+    return (
+        <div>
+            hello world
+        </div>
+    )
+}
+
+export default Quotes
